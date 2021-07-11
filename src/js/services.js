d3d4a40d4132fddac9a3e868b9a6df724ceb5bf0
Alejandro R. Sedeño <asedeno@mit.edu>
2021-07-11 14:31:15 -0400

    move bits around some more

---
+++ src/js/services.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const krb = require('./krb.js');
+
+// Friendly names of known service principals.
+// TODO(davidben): Cross-realm?
+// TODO(davidben): Move this to a config.js.
+var SERVICES = { };
+SERVICES["krbtgt/" + krb.realm + "@" + krb.realm] = {
+    dangerous: true,
+    desc: "Full access to your Athena account"
+};
+SERVICES["moira/moira7.mit.edu" + "@" + krb.realm] = {
+    dangerous: true,
+    desc: "View and modify your mailing lists and groups"
+};
+SERVICES["afs/athena.mit.edu" + "@" + krb.realm] = {
+    dangerous: true,
+    desc: "Full access to all your files on Athena"
+};
+SERVICES["zephyr/zephyr" + "@" + krb.realm] = {
+    desc: "Send and receive zephyr notices as you"
+};
+
+module.exports = SERVICES;
+
+
