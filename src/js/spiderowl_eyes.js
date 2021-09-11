b98c4faccce6379bbc715dccfaf121812a74ceb2
Alejandro R. Sedeño <asedeno@mit.edu>
2021-09-11 14:10:46 -0400

    spiderowl_eyes: add eye placement style on window load again
    Putting the styling directly into the eye elements runs afowl of CSP
    unless we add 'unsafe-inline' to style-src.

--- src/js/spiderowl_eyes.js
+++ src/js/spiderowl_eyes.js
@@ -1,3 +1,17 @@
+window.addEventListener('load', () => {
+  const locMap = {
+    'eye1': { left: '78px', top: '12px' },
+    'eye2': { left: '87px', top: '16px' },
+    'eye3': { left: '105px', top: '16px' },
+    'eye4': { left: '121px', top: '12px' },
+  };
+  for (const eye of document.querySelectorAll('#logo img')) {
+    eye.style.left = locMap[eye.id].left;
+    eye.style.top = locMap[eye.id].top;
+    eye.removeAttribute('hidden');
+  }
+});
+
 document.addEventListener('mousemove', event => {
   for (const eye of document.querySelectorAll('#logo img')) {
     const {left, top} = eye.getBoundingClientRect();
